b46702b290aa9e3e6cf35ec86cbd700978754012
VMorsaint <morsaint_vincent@hotmail.com>
2015-12-14 23:08:51 -0500

    final changes

---
+++ app/src/main/java/com/ben/cosc3p97project/DatabaseClasses/BodyLocation.java
@@ -0,0 +1,33 @@
+/**
+ * Created by VMorsaint - 4864450 on 11/7/2015.
+ * Cosc 3p97 Assignment 2
+ */
+package com.ben.cosc3p97project.DatabaseClasses;
+
+import android.provider.BaseColumns;
+
+
+public class BodyLocation implements BaseColumns
+{
+    private int iLocationID;
+    private String sLocationName;
+    public static final String TABLE_NAME = "tLocations";
+    public static final String COL_LOCATION_ID = "cLocationPk";
+    public static final String COL_NAME = "cName";
+
+    public BodyLocation(String sName)
+    {
+        iLocationID = 0;
+        sLocationName = sName;
+    }
+
+    public BodyLocation(int iID, String sName)
+    {
+        iLocationID = iID;
+        sLocationName = sName;
+    }
+    public String getName()
+    {
+        return sLocationName;
+    }
+}
